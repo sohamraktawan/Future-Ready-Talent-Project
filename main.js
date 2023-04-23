e9a10e1c67a6dc5e8cf5c09b9c3e9311620bcb97
sohamraktawan <sohamraktawan@gmail.com>
2023-04-23 20:43:05 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,70 @@
+// Toggle the .pa-fixed-header class when the user 
+// scroll 100px 
+
+window.onscroll = () => {scrollNavbar()};
+
+scrollNavbar = () => {
+    // Target elements
+    const navBar = document.getElementById("nav");
+    const links = document.querySelectorAll("#nav div a");
+    const home_element = document.getElementById("nav-title");
+
+
+  if (document.documentElement.scrollTop > 100) {
+    navBar.classList.add("pa-fixed-header");
+    home_element.classList.add('text_black');
+
+    // Change the color of links on scroll
+    for (let i = 0; i < links.length; i++) {
+        const element = links[i];
+        element.classList.add('text-black');
+    }
+
+  } else {
+    navBar.classList.remove("pa-fixed-header");
+    
+    // Change the color of links back to default
+    for (let i = 0; i < links.length; i++) {
+        const element = links[i];
+        element.classList.remove('text-black');
+        home_element.classList.remove('text_black');
+    }
+
+  
+
+  }
+}
+
+const counters = document.querySelectorAll('.counter');
+const speed = 100; // The lower the slower
+
+counters.forEach(counter => {
+	const updateCount = () => {
+		const target = +counter.getAttribute('data-target');
+		const count = +counter.innerText;
+
+		// Lower inc to slow and higher to slow
+		const inc = target / speed;
+
+		// console.log(inc);
+		// console.log(count);
+
+		// Check if target is reached
+		if (count < target) {
+			// Add inc to count and output in counter
+			counter.innerText = count + inc;
+			// Call function every ms
+			setTimeout(updateCount, 1);
+		} else {
+			counter.innerText = target;
+		}
+
+	};
+
+	updateCount();
+});
+
+
+
+
+
